c01e82205ec0b0e0a411fd0bc40c922f7067eab8
Bipul <bipulray56@gmail.com>
2026-04-13 22:56:11 +0530

    Files Reorganised

--- GitMain.java
+++ GitMain.java
@@ -1,13 +1,8 @@
 public class GitMain {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("GitMain Class");
         Games obj=new Games();
         obj.game();
     }
-
-    void car(){
-        System.out.println("nexon");
-    }
-
 }
