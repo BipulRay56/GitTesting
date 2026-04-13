a3bd9766f9ebe534ac8b4bd45ea2b502ced4bf33
Bipul <bipulray56@gmail.com>
2026-04-13 22:49:33 +0530

    ading animal in  branch

--- GitMain.java
+++ GitMain.java
@@ -3,11 +3,12 @@ public class GitMain {
     public static void main(String[] args) {
         System.out.println("hello world");
         Games obj=new Games();
+        animal foo = new animal();
         obj.game();
+        foo.m();
     }
 
     void car(){
         System.out.println("nexon");
     }
-
 }
---
+++ animal.java
@@ -0,0 +1,5 @@
+public class animal {
+    void m(){
+        System.out.println("i am craeted in animal branch");
+    }
+}
